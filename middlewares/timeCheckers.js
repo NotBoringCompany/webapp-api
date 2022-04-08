c0a123d9f002bb77cbcad09f3dab076629b65b5f
shonsirsha <seangeekpro@gmail.com>
2022-04-08 20:24:23 +0200

    currentTime in seconds

--- middlewares/timeCheckers.js
+++ middlewares/timeCheckers.js
@@ -1,14 +1,18 @@
 const publicMintTime = async function (_, res, next) {
 	const openTime = parseInt(process.env.PUBLIC_MINT_OPENS_TIMESTAMP);
 	const closeTime = parseInt(process.envPUBLIC_MINT_CLOSES_TIMESTAMP);
-	const currentTime = Date.now();
+	const currentTime = Math.round(Date.now() / 1000);
 
 	//Between opening time and closing time ...
 	if (currentTime >= openTime && currentTime <= closeTime) {
 		next(); // next() here means it will go to the actual route
 	} else {
 		//Otherwise, forbidden to access route.
-		res.status(403).json({ errorMessage: "Public minting isn't open" });
+		res.status(403).json({
+			errorMessage: "Public minting isn't open",
+			currentTime,
+			openTime,
+		});
 	}
 };
 
