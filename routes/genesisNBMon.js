95d8700071fa7213571afa093096cba072595577
shonsirsha <seangeekpro@gmail.com>
2022-04-08 10:59:30 +0200

    use double-quotation

--- routes/genesisNBMon.js
+++ routes/genesisNBMon.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const genesisLogic = require('../logic/genesisNBMonLogic');
+const genesisLogic = require("../logic/genesisNBMonLogic");
 
-router.get('/getGenesisNBMon/:id', async (req, res) => {
-    let id = req.params.id;
-    let nbmon = await genesisLogic.getGenesisNBMon(id).catch((err) => res.json(err.message));
-    res.json(nbmon);
-})
+router.get("/getGenesisNBMon/:id", async (req, res) => {
+	let id = req.params.id;
+	let nbmon = await genesisLogic
+		.getGenesisNBMon(id)
+		.catch((err) => res.json(err.message));
+	res.json(nbmon);
+});
 
 module.exports = router;
