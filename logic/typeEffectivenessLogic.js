f4ce49ba7a7519b5c1c47682561253c26953c49c
juandogels <suwandresukijat@gmail.com>
2022-04-21 23:11:03 +0200

    removed Moralis.start from functions

--- logic/typeEffectivenessLogic.js
+++ logic/typeEffectivenessLogic.js
@@ -26,8 +26,6 @@ let allTypes = [
 // get nbmon's strengths and weaknesses against other types when attacking
 const getAttackEffectiveness = async (firstType, secondType, isGenesis) => {
     try {
-        await Moralis.start({ serverUrl, appId, masterKey });
-
         // defense and attack effectiveness are stored in separate databases
         const attackQuery = new Moralis.Query("Types_Attack");
 
@@ -98,8 +96,6 @@ const getAttackEffectiveness = async (firstType, secondType, isGenesis) => {
 // get nbmon's defense resistance and vulnerability when defending
 const getDefenseEffectiveness = async (firstType, secondType, isGenesis) => {
     try {
-        await Moralis.start({ serverUrl, appId, masterKey });
-
         const defenseQuery = new Moralis.Query("Types_Defense");
 
         let nbmonResistantTo = [];
