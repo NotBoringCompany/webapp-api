8a4afd3fecc583f0941e59430317cd723b050275
juandogels <suwandresukijat@gmail.com>
2022-04-24 22:07:01 +0200

    implemented uuid for hatching stats

--- logic/genesisNBMonHatchingLogic.js
+++ logic/genesisNBMonHatchingLogic.js
@@ -1,9 +1,30 @@
+const { v4: uuidv4 } = require('uuid');
+const ethers = require("ethers");
+const fs = require("fs");
+const path = require("path");
+
+const moralisAPINode = process.env.MORALIS_APINODE;
+// rinkeby URL connected with Moralis
+const nodeURL = `https://speedy-nodes-nyc.moralis.io/${moralisAPINode}/eth/rinkeby`;
+const customHttpProvider = new ethers.providers.JsonRpcProvider(nodeURL);
+const genesisNBMonABI = fs.readFileSync(
+	path.resolve(__dirname, "../abi/genesisNBMon.json")
+);
+const genesisABI = JSON.parse(genesisNBMonABI);
+const genesisContract = new ethers.Contract(
+	process.env.CONTRACT_ADDRESS,
+	genesisABI,
+	customHttpProvider
+);
+
 const genesisStatRandomizer = require("../calculations/genesisStatRandomizer");
 const { getGenesisNBMonTypes } = require("./genesisNBMonLogic");
 
 // hatches the nbmon from an egg and gives it its respective stats
 const randomizeHatchingStats = async () => {
     try {
+        const key = uuidv4();
+        const signer = new ethers.Wallet(pvtKey, customHttpProvider);
         const gender = (await genesisStatRandomizer.randomizeGenesisGender()).toString();
         const rarity = (await genesisStatRandomizer.randomizeGenesisRarity()).toString();
         const genus = (await genesisStatRandomizer.randomizeGenesisGenus()).toString();
@@ -15,12 +36,35 @@ const randomizeHatchingStats = async () => {
         const potential = await genesisStatRandomizer.randomizeGenesisPotential(rarity);
         const passives = await genesisStatRandomizer.randomizeGenesisPassives();
 
-        return {
-            nbmonStats: nbmonStats,
-            types: types,
-            potential: potential,
-            passives: passives
-        }
+        let unsignedTx = await genesisContract
+            .populateTransaction.addValidKey(
+                key, 
+                nbmonStats, 
+                types, 
+                potential, 
+                passives
+            );
+        let response = await signer.sendTransaction(unsignedTx);
+        let minedResponse = await response.wait();
+
+        //Turns response to string, and turn it back to JSON
+		//This is done because for some reason response is a ParseObject and not a JSON
+		const jsonResponse = JSON.parse(JSON.stringify(response));
+		//Read about ParseObject: https://parseplatform.org/Parse-SDK-JS/api/master/Parse.Object.html
+		//Parseplatform is used by Moralis' DB
+
+		//Upon successful minting
+		await addToActivities(
+			jsonResponse.hash,
+			"genesisHatching",
+			"eth",
+			0
+		);
+
+		return {
+            response: minedResponse,
+            key: key
+        };
     } catch (err) {
         return err;
     }
--- routes/genesisNBMonHatching.js
+++ routes/genesisNBMonHatching.js
@@ -2,9 +2,8 @@ const express = require("express");
 const { randomizeHatchingStats } = require("../logic/genesisNBMonHatchingLogic");
 const router = express.Router();
 
-router.get("/randomizeHatchingStats", async (_, res) => {
-    let randomizer = await randomizeHatchingStats().catch((err) => res.json(err));
-    res.json(randomizer);
+router.post("/hatchNBMon", async (_, res) => {
+    let hatchNBMon = randomizeHatchingStats();
 });
 
 module.exports = router;
