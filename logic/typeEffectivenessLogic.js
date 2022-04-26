61c59f9d8a1c7fa7dec39a5d9a22305471071f85
juandogels <suwandresukijat@gmail.com>
2022-04-26 20:50:20 +0200

    removed isGenesis and refactored getAttackEffectiveness and getDefenseEffectiveness

--- logic/typeEffectivenessLogic.js
+++ logic/typeEffectivenessLogic.js
@@ -22,21 +22,8 @@ let allTypes = [
  */
 const parseJSON = (data) => JSON.parse(JSON.stringify(data));
 
-const testQuery = async () => {
+const getAttackEffectiveness = async (firstType, secondType) => {
     try {
-        const attackQuery = Moralis.Object.extend("Types_Attack");
-        const query = new Moralis.Query(attackQuery);
-        query.equalTo("Attacking_Type", "Ordinary")
-        const res = await query.find();
-        return parseJSON(res);
-    } catch (err) {
-        return err;
-    }
-}
-
-const getAttackEffectiveness = async (firstType, secondType, isGenesis) => {
-    try {
-
         if (secondType === "null") {
             secondType = null;
         }
@@ -51,188 +38,98 @@ const getAttackEffectiveness = async (firstType, secondType, isGenesis) => {
         let nbmonStrongAgainst = [];
         let nbmonWeakAgainst = [];
 
-        // if nbmon is genesis, we use genesisContract.
-        if (isGenesis === "true") {
-            // if nbmon has two types
-            if (firstType !== null && secondType !== null) {
-                allTypes.forEach((receivingType, index) => {
-                    let idx = Array.from(parseJSON(firstTypeQueryRes)).findIndex(type => type["Receiving_Type"] === receivingType);
-                    let secondIdx = Array.from(parseJSON(secondTypeQueryRes)).findIndex(type => type.Receiving_Type === receivingType);
-                    console.log(secondIdx);
-                    let firstTypeAttackEff = parseFloat(parseJSON(firstTypeQueryRes[idx])["Effectiveness"]);
-                    let secondTypeAttackEff = parseFloat(parseJSON(secondTypeQueryRes[secondIdx])["Effectiveness"]);
-                    let attackEff = firstTypeAttackEff * secondTypeAttackEff;
+        // if nbmon has two types
+        if (firstType !== null && secondType !== null) {
+            allTypes.forEach((receivingType) => {
+                let idx = parseJSON(firstTypeQueryRes).findIndex((type) => type["Receiving_Type"] === receivingType);
+                let secondIdx = parseJSON(secondTypeQueryRes).findIndex(type => type["Receiving_Type"] === receivingType);
+                let firstTypeAttackEff = parseFloat(parseJSON(firstTypeQueryRes[idx])["Effectiveness"]);
+                let secondTypeAttackEff = parseFloat(parseJSON(secondTypeQueryRes[secondIdx])["Effectiveness"]);
+                let attackEff = firstTypeAttackEff * secondTypeAttackEff;
+                if (attackEff > 1) {
+                    nbmonStrongAgainst.push(receivingType);
+                } else if (attackEff < 1) {
+                    nbmonWeakAgainst.push(receivingType);
+                }
+            });
+        // if nbmon has only one type
+        } else if (firstType !== null && secondType === null) {
+            allTypes.forEach((receivingType) => {
+                let idx = parseJSON(firstTypeQueryRes).findIndex(type => type["Receiving_Type"] === receivingType);
+                let attackEff = parseFloat(parseJSON(firstTypeQueryRes[idx])["Effectiveness"]);
 
-                    console.log(attackEff);
-                    
-                    if (attackEff > 1) {
-                        nbmonStrongAgainst.push(type);
-                    } else {
-                        nbmonWeakAgainst.push(type);
-                    }
-                })
-            // if nbmon has only one type
-            } else if (firstType !== null && secondType === null) {
-                allTypes.forEach((type, index) => {
-                    let attackEff = parseFloat(parseJSON(firstTypeQueryRes[index])["Effectiveness"]);
-                    console.log(parseJSON(firstTypeQueryRes[index]), type, attackEff);
-                    if (attackEff > 1) {
-                        nbmonStrongAgainst.push(type);
-                    } else {
-                        nbmonWeakAgainst.push(type);
-                    }
-                })
-            // if the nbmon is still an egg, both first and second type will be null.
-            // this doesn't check if first type doesn't exist and second type exists since that would never happen.
-            } else {
-                return "Still an egg. Please hatch the NBMon to show its attack effectiveness.";
-            }
-            console.log(nbmonStrongAgainst, nbmonWeakAgainst);
-        // if nbmon is not genesis, then we will use the normal nbmon contract.
+                if (attackEff > 1) {
+                    nbmonStrongAgainst.push(receivingType);
+                } else if (attackEff < 1) {
+                    nbmonWeakAgainst.push(receivingType);
+                }
+            })
+        // if the nbmon is still an egg, both first and second type will be null.
+        // this doesn't check if first type doesn't exist and second type exists since that would never happen.
         } else {
-            return "NBMon is not Genesis. Code not implemented yet.";
+            return "Still an egg. Please hatch the NBMon to show its attack effectiveness.";
+        }
+        return {
+            'Strong against': nbmonStrongAgainst,
+            'Weak against': nbmonWeakAgainst
         }
     } catch (err) {
         return err;
     }
 }
 
-// // get nbmon's strengths and weaknesses against other types when attacking
-// const getAttackEffectiveness = async (firstType, secondType, isGenesis) => {
-//     try {
-//         // defense and attack effectiveness are stored in separate databases
-//         const attackQuery = new Moralis.Query("Types_Attack");
-
-//         let nbmonStrongAgainst = [];
-//         let nbmonWeakAgainst = [];
-        
-//         // if nbmon is genesis, we use genesisContract.
-//         if (isGenesis === "true") {
-//             // if both first and second types exist
-//             if (firstType !== null && secondType !== null) {
-//                 for (let i = 0; i < allTypes.length; i++) {
-//                     let firstTypeAttackPipeline = [
-//                         { match: { Attacking_Type: firstType, Receiving_Type: allTypes[i] } },
-//                         { project: { _id: 0, Attacking_Type: 1, Receiving_Type: 1, Effectiveness: 1 } }
-//                     ];
-//                     let secondTypeAttackPipeline = [
-//                         { match: { Attacking_Type: secondType, Receiving_Type: allTypes[i] } },
-//                         { project: { _id: 0, Attacking_Type: 1, Receiving_Type: 1, Effectiveness: 1 } }
-//                     ];
-
-//                     const firstTypeAttackAggRes = await attackQuery.aggregate(firstTypeAttackPipeline);
-//                     const secondTypeAttackAggRes = await attackQuery.aggregate(secondTypeAttackPipeline);
-
-//                     let attackEff = firstTypeAttackAggRes[0]["Effectiveness"] * secondTypeAttackAggRes[0]["Effectiveness"];
-
-//                     if (attackEff > 1) {
-//                         nbmonStrongAgainst.push(allTypes[i]);
-//                     } else if (attackEff < 1) {
-//                         nbmonWeakAgainst.push(allTypes[i]);
-//                     }
-//                 }
-//             // if first type exists but second type doesn't
-//             } else if (firstType !== null && secondType === null) {
-//                 for (let i = 0; i < allTypes.length; i++) {
-//                     let firstTypeAttackPipeline = [
-//                         { match: { Attacking_Type: firstType, Receiving_Type: allTypes[i] } },
-//                         { project: { _id: 0, Attacking_Type: 1, Receiving_Type: 1, Effectiveness: 1 } }
-//                     ];
-
-//                     const firstTypeAttackAggRes = await attackQuery.aggregate(firstTypeAttackPipeline);
-
-//                     if (firstTypeAttackAggRes[0]["Effectiveness"] > 1) {
-//                         nbmonStrongAgainst.push(allTypes[i]);
-//                     } else if (firstTypeAttackAggRes[0]["Effectiveness"] < 1) {
-//                         nbmonWeakAgainst.push(allTypes[i]);
-//                     }
-//                 }
-//             // if the nbmon is still an egg, both first and second type will be null.
-//             // this doesn't check if first type doesn't exist and second type exists since that would never happen.
-//             } else {
-//                 return "Still an egg. Please hatch the NBMon to show its attack effectiveness.";
-//             }
-
-//             return {
-//                 'Strong against': nbmonStrongAgainst,
-//                 'Weak against': nbmonWeakAgainst
-//             }
-//         // if nbmon is not genesis, then we will use the normal nbmon contract.
-//         } else {
-//             // contract not ready yet. when ready, it will be implemented here.
-//             return "NBMon is not Genesis. Code not implemented yet.";
-//         }
-//     } catch (err) {
-//         return err;
-//     }
-// }
-
 // get nbmon's defense resistance and vulnerability when defending
-const getDefenseEffectiveness = async (firstType, secondType, isGenesis) => {
+const getDefenseEffectiveness = async (firstType, secondType) => {
     try {
-        const defenseQuery = new Moralis.Query("Types_Defense");
+        if (secondType === "null") {
+            secondType = null;
+        }
+        const TypesDefense = Moralis.Object.extend("Types_Defense");
+        const attackQuery = new Moralis.Query(TypesDefense);
+        const firstTypeQuery = attackQuery.equalTo("Receiving_Type", firstType);
+        const firstTypeQueryRes = await firstTypeQuery.find({ useMasterKey: true });
+        const secondTypeQuery = secondType !== null ? attackQuery.equalTo("Receiving_Type", secondType) : null;
+        const secondTypeQueryRes = secondTypeQuery !== null 
+            ? await secondTypeQuery.find({ useMasterKey: true }) 
+            : null;
 
         let nbmonResistantTo = [];
         let nbmonVulnerableTo = [];
 
-        if (isGenesis === "true") {
-            // if both first and second types exist
-            if (firstType !== null && secondType !== null) {
-                for (let i = 0; i < allTypes.length; i++) {
-                    let firstTypeDefensePipeline = [
-                        { match: { Receiving_Type: firstType, Attacking_Type: allTypes[i] } },
-                        { project: { _id: 0, Receiving_Type: 1, Attacking_Type: 1, Effectiveness: 1 } }
-                    ];
-                    let secondTypeDefensePipeline = [
-                        { match: { Receiving_Type: secondType, Attacking_Type: allTypes[i] } },
-                        { project: { _id: 0, Receiving_Type: 1, Attacking_Type: 1, Effectiveness: 1 } }
-                    ];
-
-                    const firstTypeDefenseAggRes = await defenseQuery.aggregate(firstTypeDefensePipeline);
-                    const secondTypeDefenseAggRes = await defenseQuery.aggregate(secondTypeDefensePipeline);
-
-                    let defenseEff = firstTypeDefenseAggRes[0]["Effectiveness"] * secondTypeDefenseAggRes[0]["Effectiveness"];
-
-                    if (defenseEff < 1) {
-                        nbmonResistantTo.push(allTypes[i]);
-                    } else if (defenseEff > 1) {
-                        nbmonVulnerableTo.push(allTypes[i]);
-                    }
+        if (firstType !== null && secondType !== null) {
+            allTypes.forEach((attackingType) => {
+                let idx = parseJSON(firstTypeQueryRes).findIndex((type) => type["Attacking_Type"] === attackingType);
+                let secondIdx = parseJSON(secondTypeQueryRes).findIndex(type => type["Attacking_Type"] === attackingType);
+                let firstTypeDefenseEff = parseFloat(parseJSON(firstTypeQueryRes[idx])["Effectiveness"]);
+                let secondTypeDefenseEff = parseFloat(parseJSON(secondTypeQueryRes[secondIdx])["Effectiveness"]);
+                let defenseEff = firstTypeDefenseEff * secondTypeDefenseEff;
+                if (defenseEff < 1) {
+                    nbmonResistantTo.push(attackingType);
+                } else if (defenseEff > 1) {
+                    nbmonVulnerableTo.push(attackingType);
                 }
-            // if nbmon only has one type
-            } else if (firstType !== null && secondType === null) {
-                for (let i = 0; i < allTypes.length; i++) {
-                    let firstTypeDefensePipeline = [
-                        { match: { Receiving_Type: firstType, Attacking_Type: allTypes[i] } },
-                        { project: { _id: 0, Receiving_Type: 1, Attacking_Type: 1, Effectiveness: 1 } }
-                    ];
+            });
+        } else if (firstType !== null && secondType === null) {
+            allTypes.forEach((attackingType) => {
+                let idx = parseJSON(firstTypeQueryRes).findIndex(type => type["Attacking_Type"] === attackingType);
+                let defenseEff = parseFloat(parseJSON(firstTypeQueryRes[idx])["Effectiveness"]);
 
-                    const firstTypeDefenseAggRes = await defenseQuery.aggregate(firstTypeDefensePipeline);
-
-                    if (firstTypeDefenseAggRes[0]["Effectiveness"] < 1) {
-                        nbmonResistantTo.push(allTypes[i]);
-                    } else if (firstTypeDefenseAggRes[0]["Effectiveness"] > 1) {
-                        nbmonVulnerableTo.push(allTypes[i]);
-                    }
+                if (defenseEff < 1) {
+                    nbmonResistantTo.push(attackingType);
+                } else if (defenseEff > 1) {
+                    nbmonVulnerableTo.push(attackingType);
                 }
-            // if the nbmon is still an egg, both first and second type will be null.
-            // this doesn't check if first type doesn't exist and second type exists since that would never happen.
-            } else {
-                return "Still an egg. Please hatch the NBMon to show its defense effectiveness.";
-            }
-
-            return {
-                'Resistant to': nbmonResistantTo,
-                'Vulnerable to': nbmonVulnerableTo
-            }
-        // if nbmon is not genesis, then we will use the normal nbmon contract.
+            })
         } else {
-            // contract not ready yet. when ready, it will be implemented here.
-            return "NBMon is not Genesis. Code not implemented yet.";
+            return "Still an egg. Please hatch the NBMon to show its attack effectiveness.";
+        }
+        return {
+            'Resistant to' : nbmonResistantTo,
+            'Vulnerable to': nbmonVulnerableTo
         }
     } catch (err) {
         return err;
     }
 }
 
-module.exports = { getAttackEffectiveness, getDefenseEffectiveness, testQuery };
+module.exports = { getAttackEffectiveness, getDefenseEffectiveness };
--- routes/typeEffectiveness.js
+++ routes/typeEffectiveness.js
@@ -3,27 +3,20 @@ const router = express.Router();
 
 const { getAttackEffectiveness, getDefenseEffectiveness, testQuery } = require("../logic/typeEffectivenessLogic");
 
-router.get("/getAttackEffectiveness/:firstType/:secondType/:isGenesis", async (req, res) => {
+router.get("/getAttackEffectiveness/:firstType/:secondType", async (req, res) => {
     let firstType = req.params.firstType;
     let secondType = req.params.secondType;
-    let isGenesis = req.params.isGenesis;
 
-    let effectiveness = await getAttackEffectiveness(firstType, secondType, isGenesis).catch((err) => res.json(err.message));
+    let effectiveness = await getAttackEffectiveness(firstType, secondType).catch((err) => res.json(err.message));
     res.json(effectiveness);
 });
 
-router.get("/getDefenseEffectiveness/:firstType/:secondType/:isGenesis", async (req, res) => {
+router.get("/getDefenseEffectiveness/:firstType/:secondType", async (req, res) => {
     let firstType = req.params.firstType;
     let secondType = req.params.secondType;
-    let isGenesis = req.params.isGenesis;
 
-    let effectiveness = await getDefenseEffectiveness(firstType, secondType, isGenesis).catch((err) => res.json(err.message));
+    let effectiveness = await getDefenseEffectiveness(firstType, secondType).catch((err) => res.json(err.message));
     res.json(effectiveness);
 });
-
-router.get("/testQuery", async (req, res) =>{
-    let test = await testQuery().catch((err) => res.json(err));
-    res.json(test);
-})
 
 module.exports = router;
