892a66b939b5f47e6f9032a1c81d9482cd072162
juandogels <suwandresukijat@gmail.com>
2022-04-29 11:50:22 +0200

    installed and imported aws-sdk, initialized S3 and created uploadGenesisEggMetadata

---
+++ logic/genesisMetadataLogic.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const ethers = require("ethers");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
+
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint.href, 
+    credentials: new AWS.Credentials({
+        accessKeyId: process.env.SPACES_KEY,
+        secretAccessKey: process.env.SPACES_SECRET
+    })
+});
+
+/**
+ * @dev This will upload the metadata of the EGG to Spaces. Will contain mostly empty stats/attributes.    
+ * @param {id} id resembles the NBMon ID of the metadata to be created for.
+ */
+const uploadGenesisEggMetadata = (id) => {
+
+    // contains the metadata of the egg
+    const metadata = {
+        "name": `NBMon Egg #${id}`,
+        "description": "This egg contains a mysterious NBMon. Hatch it to find out.",
+        "image": "https://nbcompany.fra1.digitaloceanspaces.com/genesisEgg.png"
+    };
+
+    s3.putObject({
+        Bucket: process.env.SPACES_NAME,
+        Key: `genesisNBMon/${id}.json`,
+        Body: JSON.stringify(metadata),
+        ACL: "public-read",
+        ContentType: "application/json"
+    }, (err, data) => {
+        if (err) throw new Error(err.stack);
+        console.log("File upploaded successfully!", data);
+    });
+}
+
+module.exports = {
+    uploadGenesisEggMetadata
+};
