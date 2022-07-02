9c10f36cd67cdd93962b3fbdb8beafc4a63ae153
shonsirsha <seangeekpro@gmail.com>
2022-07-02 19:47:17 +0200

    Better handle error in some endpoints to prevent server crashing

--- routes/genesisNBMon.js
+++ routes/genesisNBMon.js
@@ -2,50 +2,81 @@ const express = require("express");
 const router = express.Router();
 
 const genesisLogic = require("../logic/genesisNBMonLogic");
+const httpErrorStatusCode = require("../utils/httpErrorStatusCode");
 
 router.get("/getGenesisNBMon/:id", async (req, res) => {
-	let id = req.params.id;
-	let nbmon = await genesisLogic
-		.getGenesisNBMon(id)
-		.catch((err) => res.json(err.message));
-	res.json(nbmon);
+	try {
+		let id = req.params.id;
+		let nbmon = await genesisLogic.getGenesisNBMon(id).catch((err) => {
+			throw err;
+		});
+		res.json(nbmon);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 router.get("/getOwnerGenesisNBMonIDs/:address", async (req, res) => {
-	let address = req.params.address;
-	let ownerIds = await genesisLogic
-		.getOwnerGenesisNBMonIDs(address)
-		.catch((err) => res.json(err.message));
-	res.json(ownerIds);
+	try {
+		let address = req.params.address;
+		let ownerIds = await genesisLogic
+			.getOwnerGenesisNBMonIDs(address)
+			.catch((err) => {
+				throw err;
+			});
+		res.json(ownerIds);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 router.get("/getOwnerGenesisNBMons/:address", async (req, res) => {
-	let address = req.params.address;
-	let nbmons = await genesisLogic
-		.getOwnerGenesisNBMons(address)
-		.catch((err) => res.json(err.message));
-	res.json(nbmons);
+	try {
+		let address = req.params.address;
+		let nbmons = await genesisLogic
+			.getOwnerGenesisNBMons(address)
+			.catch((err) => {
+				throw err;
+			});
+		res.json(nbmons);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 router.get("/config/:address", async (req, res) => {
-	let address = req.params.address;
-	let config = await genesisLogic
-		.config(address)
-		.catch((err) => res.json(err.message));
-	res.json(config);
+	try {
+		let address = req.params.address;
+		let config = await genesisLogic.config(address).catch((err) => {
+			throw err;
+		});
+		res.json(config);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 router.get("/config", async (_, res) => {
-	let config = await genesisLogic
-		.generalConfig()
-		.catch((err) => res.json(err.message));
-	res.json(config);
+	try {
+		let config = await genesisLogic.generalConfig().catch((err) => {
+			throw err;
+		});
+		res.json(config);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 router.get("/getTypes/:genus", async (req, res) => {
-	let genus = req.params.genus;
-	let types = await genesisLogic.getGenesisNBMonTypes(genus).catch((err) => res.json(err));
-	res.json(types);
+	try {
+		let genus = req.params.genus;
+		let types = await genesisLogic.getGenesisNBMonTypes(genus).catch((err) => {
+			throw err;
+		});
+		res.json(types);
+	} catch (error) {
+		res.status(httpErrorStatusCode(error.code)).json({ error });
+	}
 });
 
 module.exports = router;
